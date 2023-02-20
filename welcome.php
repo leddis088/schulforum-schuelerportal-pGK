5319546270b7cf337c02a60dc5aaa8e6f8f3461a
Leddis088 <leon.m.2001@gmx.de>
2023-02-20 13:31:47 +0100

    welcome.php added

---
+++ welcome.php
@@ -0,0 +1,12 @@
+<?php
+  session_start();
+
+  if (!isset($_SESSION['username'])) {
+    header('Location: login.php');
+    exit();
+  }
+
+  echo "<h1>Hallo, " . $_SESSION['username'] ,du hast erfolgreich einen Account erstellt. Wir wünschen dir viel Spaß auf dem Schulforum von Tobias Löffler, Felix Riemer und Leon Kruspel. "!</h1>";
+
+  echo "<form action='logout.php'><button type='submit'>Logout</button></form>";
+?>
