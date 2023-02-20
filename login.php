a31aeb637db966d182bfd1e32221f5cc9b4e04b2
Leddis088 <leon.m.2001@gmx.de>
2023-02-20 13:35:47 +0100

    login fix

--- login.php
+++ login.php
@@ -1,3 +1,6 @@
+<?php
+  session_start(); // Start the session
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,25 +11,28 @@
   <h1>Login</h1>
 
   <?php
-
+    // If the user submits the form
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
+      // Connect to the database
       $db = mysqli_connect('localhost', 'forum_user', 'Felix123!', 'forum_db');
 
+      // Get the username and password from the form
       $username = mysqli_real_escape_string($db, $_POST['username']);
       $password = mysqli_real_escape_string($db, $_POST['password']);
 
-      $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+      // Find the user in the database
+      $query = "SELECT * FROM users WHERE username='$username'";
       $result = mysqli_query($db, $query);
       $user = mysqli_fetch_assoc($result);
 
-      if ($user) {
-        session_start();
+      // Verify the password
+      if ($user && password_verify($password, $user['password'])) {
+        // If the password is correct, log the user in and redirect to the home page
         $_SESSION['username'] = $username;
-        header('Location: welcome.php');
+        header('Location: home.html');
         exit();
       } else {
-
+        // If the password is incorrect, display an error message
         echo "<p>Invalid username or password</p>";
       }
     }
