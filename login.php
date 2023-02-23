bdff612d5b96296e8713f89045f99875b73dc936
leddis088 <leon.m.2001@gmx.de>
2023-02-23 12:02:12 +0100

    login finishd

--- login.php
+++ login.php
@@ -9,50 +9,48 @@
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') {	
       $username = $_POST['username'];
       $password = $_POST['password'];
-
       $servername = 'localhost';
       $dbusername = 'forum_user';
       $dbpassword = 'Felix123!';
       $dbname = 'forum_db';
-  
+    
       $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
-
+    
       if ($conn->connect_error) {
-        die('Die Verbindung zur Datenbank wurde unterbrochen!');
+        die('Connection failed: ' . $conn->connect_error);
       }
-
+    
       $sql = "SELECT * FROM users WHERE username = '$username'";
       $result = $conn->query($sql);
-  
+    
       if ($result->num_rows > 0) {
         $user = $result->fetch_assoc();
-        if (password_verify($password, $user['password'])) {
-
+        if (password_verify($password, $user['hash'])) {
+    
           header('Location: home.html');
           exit();
         } else {
-          echo 'Falsches Passwort!';
+          echo 'Incorrect password!';
         }
       } else {
-        echo 'Benutzername nicht gefunden!';
+        echo 'Username not found!';
       }
-
+    
       $conn->close();
-  
+    
     }
-  ?>
+    ?>
+
+    <h1>Login</h1>
+    <form method="post" action="">
+      <label>Username:</label>
+      <input type="text" name="username" required><br><br>
+      <label>Password:</label>
+      <input type="password" name="password" required><br><br>
+      <input type="submit" value="Login">
+    </form>
+    <br>
+  <a href="register.php">Register</a>
   
-  <h1>Login</h1>
-  
-  <form method="post" action="">
-    <label>Benutzername:</label>
-    <input type="text" name="username" required><br><br>
-    <label>Passwort:</label>
-    <input type="password" name="password" required><br><br>
-    <input type="submit" value="Anmelden">
-  </form>
-  
-  <br>
-  
-  <a href="register.php">Registrieren</a>
-  
+  </body>
+</html>    
