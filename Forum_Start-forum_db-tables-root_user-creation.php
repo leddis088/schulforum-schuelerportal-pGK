ab963564d3ce7163642a74b13ce9f6a26fe1f88c
leddis088 <leon.m.2001@gmx.de>
2023-02-23 11:40:32 +0100

    datastructure update

--- Forum_Start-forum_db-tables-root_user-creation.php
+++ Forum_Start-forum_db-tables-root_user-creation.php
@@ -1,9 +1,7 @@
 <?php
-
 $servername = "localhost";
 $username = "forum_user";
 $password = "Felix123!";
-$dbname = "forum_db";
 
 $conn = new mysqli($servername, $username, $password);
 
@@ -11,87 +9,76 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-if (!$conn->select_db($dbname)) {
-
-    $sql = "CREATE DATABASE $dbname";
-    if ($conn->query($sql) === TRUE) {
-        echo "Database created successfully";
-    } else {
-        echo "Error creating database: " . $conn->error;
-    }
+$sql = "CREATE DATABASE IF NOT EXISTS forum_db";
+if ($conn->query($sql) !== TRUE) {
+    die("Error creating database: " . $conn->error);
 }
 
-$conn->select_db($dbname);
+$conn->select_db("forum_db");
 
-$sql = "CREATE TABLE IF NOT EXISTS username (
-        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(30) NOT NULL
-    )";
+$sql = "CREATE TABLE IF NOT EXISTS users (
+    _id VARCHAR(255) PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    class VARCHAR(30) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    hash VARCHAR(255) NOT NULL
+)";
 $conn->query($sql);
 
-$sql = "CREATE TABLE IF NOT EXISTS first_name (
-        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(30) NOT NULL
-    )";
+$sql = "CREATE TABLE IF NOT EXISTS posts (
+    _id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    author_id VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    date_created VARCHAR(255) NOT NULL,
+    topic VARCHAR(255) NOT NULL
+)";
 $conn->query($sql);
 
-$sql = "CREATE TABLE IF NOT EXISTS last_name (
-        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(30) NOT NULL
-    )";
+$sql = "CREATE TABLE IF NOT EXISTS user_perms (
+    _id VARCHAR(255) PRIMARY KEY,
+    user_id VARCHAR(255) NOT NULL,
+    permission VARCHAR(255) NOT NULL
+)";
 $conn->query($sql);
 
-$sql = "CREATE TABLE IF NOT EXISTS class (
-        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(30) NOT NULL
-    )";
+$sql = "CREATE TABLE IF NOT EXISTS comments (
+    _id VARCHAR(255) PRIMARY KEY,
+    author_id VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    date_created VARCHAR(255) NOT NULL,
+    post_id VARCHAR(255) NOT NULL
+)";
 $conn->query($sql);
 
-$sql = "CREATE TABLE IF NOT EXISTS password (
-        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        value VARCHAR(255) NOT NULL
-    )";
-$conn->query($sql);
-
-$sql = "CREATE TABLE IF NOT EXISTS rank (
-        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(30) NOT NULL
-    )";
-$conn->query($sql);
-
-$sql = "SELECT * FROM username WHERE name='root'";
+$sql = "SELECT * FROM users WHERE username='root'";
 $result = $conn->query($sql);
 
 if ($result->num_rows == 0) {
-
     $username = 'root';
+    $class = 'none';
     $first_name = 'root';
     $last_name = 'root';
-    $class = 'none';
     $password = 'felix123';
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-    $rank = 'admin';
 
-    $sql = "INSERT INTO username (name) VALUES ('$username')";
-    $conn->query($sql);
+    $sql = "INSERT INTO users (_id, username, first_name, last_name, hash, class)
+        VALUES (UUID(), ?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sssss", $username, $first_name, $last_name, $hashed_password, $class);
+    $stmt->execute();
 
-    $sql = "INSERT INTO first_name (name) VALUES ('$first_name')";
-    $conn->query($sql);
+    $root_user_id = $conn->insert_id;
 
-    $sql = "INSERT INTO last_name (name) VALUES ('$last_name')";
-    $conn->query($sql);
-
-    $sql = "INSERT INTO class (name) VALUES ('$class')";
-    $conn->query($sql);
-
-    $sql = "INSERT INTO password (value) VALUES ('$hashed_password')";
-    $conn->query($sql);
-
-    $sql = "INSERT INTO rank (name) VALUES ('$rank')";
-    $conn->query($sql);
+    $sql = "INSERT INTO user_perms (_id, user_id, permission) VALUES (UUID(), ?, 'admin')";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $root_user_id);
+    $stmt->execute();
 
     echo "Root user created successfully";
 }
 
 $conn->close();
+
 ?>
