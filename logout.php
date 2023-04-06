37a89e28f8f5b6ec669a07951d4e2c5375aa06b4
leddis088 <leon.m.2001@gmx.de>
2023-04-06 23:59:45 +0200

    added a bug_report and made some chages

--- logout.php
+++ logout.php
@@ -5,6 +5,21 @@
 
   session_destroy();
 
-  header('Location: login.php');
+  echo "Bis zum naechsten Mal ";
+?>
+<html>
+  <script>
+  var counter = 10;
+  setInterval(function() {
+      counter--;
+      if(counter < 0) {
+          window.location = 'login.php';
+      } else {
+          document.getElementById("count").innerHTML = counter;
+          }
+  }, 1000);
+  </script>
+</html>
+<?php
   exit();
 ?>
